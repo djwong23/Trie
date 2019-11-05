68a45f615d81cd0b62b9630b0bac9974d548d0f5
Derek Wong <djwong23@gmail.com>
2019-11-04 21:36:33 -0800

    aaaaaaa

--- src/trie/Trie.java
+++ src/trie/Trie.java
@@ -22,11 +22,40 @@ public class Trie {
 	 * @return Root of trie with all words inserted from the input array
 	 */
 	public static TrieNode buildTrie(String[] allWords) {
-		/** COMPLETE THIS METHOD **/
-		
-		// FOLLOWING LINE IS A PLACEHOLDER TO ENSURE COMPILATION
-		// MODIFY IT AS NEEDED FOR YOUR IMPLEMENTATION
-		return null;
+		TrieNode root = new TrieNode(null, null, null);
+		Indexes in = new Indexes(0, (short) 0, (short) (allWords[0].length() - 1));
+		root.firstChild = new TrieNode(in, null, null);
+		if (allWords.length == 1)
+			return root;
+		for (short i = 1; i < allWords.length; i++) {
+			String ins = allWords[i];
+			TrieNode ptr = root.firstChild;
+			String word = allWords[ptr.substr.wordIndex].substring(ptr.substr.startIndex, ptr.substr.endIndex);
+			short lastIndex = -1;
+			for (short j = 0; j < ins.length(); j++) {
+				if (word.charAt(j) == ins.charAt(j))
+					lastIndex = j;
+				else
+					break;
+			}
+			if (lastIndex != -1) {
+				ptr.substr.endIndex = (short) lastIndex;
+				ptr.firstChild = new TrieNode((new Indexes(ptr.substr.wordIndex, lastIndex, ptr.substr.endIndex)), null, null);
+				ptr.firstChild.sibling = new TrieNode((new Indexes(i, lastIndex, (short) (ins.length() - 1))), null, null);
+			} else {
+				while (ptr != null) {
+					if (ptr.sibling != null)
+						ptr = ptr.sibling;
+					else {
+						in = new Indexes(i, (short) 0, (short) (allWords[i].length() - 1));
+						ptr.sibling = new TrieNode(in, null, null);
+						ptr = root.firstChild;
+						break;
+					}
+				}
+			}
+		}
+		return root;
 	}
 	
 	/**
